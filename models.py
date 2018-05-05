fa3e545bd142a3d45348b12bc88788735a7685d9
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-05 09:37:34 +0530

    Minor change

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_context
+from passlib.apps import custom_app_context
 
 Base = declarative_base()
 
@@ -13,6 +13,9 @@ class User(Base):
 	id = Column(Integer, primary_key=True)
 	username = Column(String(32), index=True)
 	password_hash = Column(String(64))
+
+	def hash_password(self, password):
+		self.password_hash = custom_app_context.hash(password)
 
 class Bagel(Base):
 	__tablename__ = 'bagel'
