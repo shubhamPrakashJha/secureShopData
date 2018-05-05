1bfb5d3e7bc4d8e9d098e283371109bf43104c65
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-05 09:56:55 +0530

    define function to register new users with username and password.

--- views.py
+++ views.py
@@ -7,7 +7,6 @@ from flask.ext.httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 
-
 engine = create_engine('sqlite:///bagelShop.db')
 
 Base.metadata.bind = engine
@@ -15,33 +14,47 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
-#ADD @auth.verify_password here
 
-#ADD a /users route here
+# ADD @auth.verify_password here
+
+# ADD a /users route here
 @app.route("/users", methods=['POST'])
 def new_user():
-    return "Route to add new users"
+	# return "Route to add new users"
+	username = request.json.get('username')
+	password = request.json.get('password')
+	user = session.query(User).filter_by(username=username).first()
+	if username is None or password is None:
+		abort(400)
+	if user is not None:
+		abort(400)
+	user = User(username=username)
+	user.hash_password(password)
+	session.add(user)
+	session.commit()
+	return jsonify(
+		{'username': user.username}
+	)
 
 
-
-@app.route('/bagels', methods = ['GET','POST'])
-#protect this route with a required login
+@app.route('/bagels', methods=['GET', 'POST'])
+# protect this route with a required login
 def showAllBagels():
-    if request.method == 'GET':
-        bagels = session.query(Bagel).all()
-        return jsonify(bagels = [bagel.serialize for bagel in bagels])
-    elif request.method == 'POST':
-        name = request.json.get('name')
-        description = request.json.get('description')
-        picture = request.json.get('picture')
-        price = request.json.get('price')
-        newBagel = Bagel(name = name, description = description, picture = picture, price = price)
-        session.add(newBagel)
-        session.commit()
-        return jsonify(newBagel.serialize)
-
+	if request.method == 'GET':
+		bagels = session.query(Bagel).all()
+		return jsonify(bagels=[bagel.serialize for bagel in bagels])
+	elif request.method == 'POST':
+		name = request.json.get('name')
+		description = request.json.get('description')
+		picture = request.json.get('picture')
+		price = request.json.get('price')
+		newBagel = Bagel(name=name, description=description, picture=picture,
+						 price=price)
+		session.add(newBagel)
+		session.commit()
+		return jsonify(newBagel.serialize)
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
